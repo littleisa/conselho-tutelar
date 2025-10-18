75f75889648298dfb46c7b0815d8d3b33d31e925
Isabella Pereira Dias <isamilu3101@gmail.com>
2025-10-18 09:44:12 -0400

    Delete menage.py directory

--- menage.py/relatorio/urls.py
+++
@@ -1,9 +0,0 @@
-# relatorios/urls.py
-from django.urls import path
-from . import views
-app_name = 'relatorios'
-
-urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
-    path('estatisticas/', views.estatisticas, name='estatisticas'),
-]
